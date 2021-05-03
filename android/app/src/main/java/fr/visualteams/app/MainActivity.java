dc48b764d4a4d3f63bd553b0100c4564319f7976
Sylvain Chauffaille <contact@sylchauf.net>
2021-05-03 14:59:03 +0200

    Change the bundli id (android)

--- android/app/src/main/java/fr/visualteams/app/MainActivity.java
+++ android/app/src/main/java/fr/visualteams/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.visualteams;
+package fr.visualteams.app;
 
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
@@ -16,6 +16,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "visualteams";
+    return "VisualTeams";
   }
 }
