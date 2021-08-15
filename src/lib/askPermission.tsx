5aaf3af37672eeafbd599859e53271480fe9ac7a
Sylvain Chauffaille <contact@sylchauf.net>
2021-08-15 08:25:55 +0200

    Add permission for ios

--- src/lib/askPermission.tsx
+++ src/lib/askPermission.tsx
@@ -1,11 +1,11 @@
 import { Platform } from 'react-native'
-import { request, PERMISSIONS, requestMultiple } from 'react-native-permissions'
+import { PERMISSIONS, requestMultiple } from 'react-native-permissions'
 
 const askPermission = async () => {
   if (Platform.OS === 'android') {
-    requestMultiple([PERMISSIONS.ANDROID.CAMERA, PERMISSIONS.ANDROID.RECORD_AUDIO]).then((result) => {
-      console.log('result:', result)
-    })
+    await requestMultiple([PERMISSIONS.ANDROID.CAMERA, PERMISSIONS.ANDROID.RECORD_AUDIO])
+  } else {
+    await requestMultiple([PERMISSIONS.IOS.CAMERA, PERMISSIONS.IOS.MICROPHONE])
   }
 }
 
